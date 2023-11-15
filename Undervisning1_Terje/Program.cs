17a5165b839400f4c0fcc9d0d741b282566ec71c
lefIngridL <90441834+lefIngridL@users.noreply.github.com>
2023-11-15 10:36:41 +0100

    ja

--- Undervisning1_Terje/Program.cs
+++ Undervisning1_Terje/Program.cs
@@ -7,11 +7,23 @@
             Console.WriteLine("Hello, World!");
             int x = 10;
             string y = $"{x}";
-            for(int i = 0 ; i < x ; i++ ) {
+            for(int i = 0 ; i < 10 ; i++ ) {
             x += i ;
 
             }
             Console.WriteLine($"er {x} og {y} fortsatt like?");
+
+            List<int> list1 = new List<int>() { 1, 2, 3 };
+            List<int> list2 = list1;
+            
+            for(int i = 0 ;i < 3 ;i++ ) {
+                Console.WriteLine(list1[i]);
+            }
+            list2[1] = 10;  
+            for(int i = 0 ;i < 3 ;i++ ) {
+                Console.WriteLine(list1[i]);
+            }
+            
         }
     }
 }
