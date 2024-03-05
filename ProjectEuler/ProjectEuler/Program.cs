43692c4013ed001961593de7d46fa8409b99f403
Ingrid <90441834+lefIngridL@users.noreply.github.com>
2024-03-05 11:26:02 +0100

    euler

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -7,4 +7,5 @@ Console.WriteLine("Hello, World!");
 //Quest3.Prime(600851475143);
 //Quest4.Pali();
 //Quest6.Square(100);
-Quest5.Multi(20);
+//Quest5.Multi(20);
+Quest7.Prime();
---
+++ ProjectEuler/ProjectEuler/Quest7.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    internal class Quest7
+    {
+        public static void Prime()
+        {
+            double num = 0;
+            double primes = 0;
+            bool flag = false;
+            double final = 0;
+
+            while (primes < 10001)
+            {
+                flag = Loop(num);
+                if (flag)
+                {
+                    if (num != 1)
+                    {
+                        primes++;
+                        //Console.WriteLine("Prime: " + num + " -- Prime counter: " + primes);
+                    }
+                    if (primes == 10001)
+                    {
+                        final = num;
+                        break;
+                    }
+                }
+
+                num++;
+            }
+            Console.WriteLine("10001th Prime number: " + final);
+        }
+        public static bool Loop(double num)
+        {
+            bool flag = false;
+            for (int j = 0; j < num; j++)
+            {
+                if (num != j && j != 1 && num % j == 0)
+                {
+                    return false;
+                }
+                else { flag = true; }
+            }
+            return flag;
+        }
+    }
+}
